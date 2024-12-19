4990c5afcbc3d8f6b5feec340340db7140bb5614
antonellagon16 <526AgonzalezMaurera@frhsd.com>
2024-12-19 10:01:55 -0500

    done

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Updated helper functions with more descriptive names for conditions
+// Capitalize the first letter of each part of the name
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 // Generate prefix based on first name length or other conditions
 function generatePrefix(firstName) {
@@ -19,172 +22,97 @@ function generatePrefix(firstName) {
 function generateFirstName(firstName) {
     const firstLetter = firstName.charAt(0).toLowerCase();
     switch (firstLetter) {
-        case 'a':
-            return 'Artemis';
-        case 'b':
-            return 'Balthazar';
-        case 'c':
-            return 'Cyrus';
-        case 'd':
-            return 'Darius';
-        case 'e':
-            return 'Eldric';
-        case 'f':
-            return 'Felix';
-        case 'g':
-            return 'Galen';
-        case 'h':
-            return 'Horace';
-        case 'i':
-            return 'Ignatius';
-        case 'j':
-            return 'Jasper';
-        case 'k':
-            return 'Kairos';
-        case 'l':
-            return 'Lysander';
-        case 'm':
-            return 'Milo';
-        case 'n':
-            return 'Nikolai';
-        case 'o':
-            return 'Orion';
-        case 'p':
-            return 'Percival';
-        case 'q':
-            return 'Quentin';
-        case 'r':
-            return 'Roderick';
-        case 's':
-            return 'Silas';
-        case 't':
-            return 'Thaddeus';
-        case 'u':
-            return 'Ulysses';
-        case 'v':
-            return 'Valerian';
-        case 'w':
-            return 'Wulfric';
-        case 'x':
-            return 'Xander';
-        case 'y':
-            return 'Yorick';
-        case 'z':
-            return 'Zacharias';
-        default:
-            return 'Alexandros';
+        case 'a': return 'Artemis';
+        case 'b': return 'Balthazar';
+        case 'c': return 'Cyrus';
+        case 'd': return 'Darius';
+        case 'e': return 'Eldric';
+        case 'f': return 'Felix';
+        case 'g': return 'Galen';
+        case 'h': return 'Horace';
+        case 'i': return 'Ignatius';
+        case 'j': return 'Jasper';
+        case 'k': return 'Kairos';
+        case 'l': return 'Lysander';
+        case 'm': return 'Milo';
+        case 'n': return 'Nikolai';
+        case 'o': return 'Orion';
+        case 'p': return 'Percival';
+        case 'q': return 'Quentin';
+        case 'r': return 'Roderick';
+        case 's': return 'Silas';
+        case 't': return 'Thaddeus';
+        case 'u': return 'Ulysses';
+        case 'v': return 'Valerian';
+        case 'w': return 'Wulfric';
+        case 'x': return 'Xander';
+        case 'y': return 'Yorick';
+        case 'z': return 'Zacharias';
+        default: return 'Alexandros';
     }
 }
 
 // Generate middle name based on the road type and favorite color
 function generateMiddleName(roadType, favoriteColor) {
-    if (roadType === 'Road') {
-        return `${favoriteColor}trail`;
-    } else if (roadType === 'Street') {
-        return `${favoriteColor}lane`;
-    } else if (roadType === 'Court') {
-        return `${favoriteColor}stone`;
-    } else if (roadType === 'Avenue') {
-        return `${favoriteColor}path`;
-    } else if (roadType === 'Boulevard') {
-        return `${favoriteColor}way`;
-    } else if (roadType === 'Drive') {
-        return `${favoriteColor}wayfarer`;
-    } else {
-        return `${favoriteColor}gate`;
-    }
+    if (roadType === 'Road') return `${favoriteColor}trail`;
+    if (roadType === 'Street') return `${favoriteColor}lane`;
+    if (roadType === 'Court') return `${favoriteColor}stone`;
+    if (roadType === 'Avenue') return `${favoriteColor}path`;
+    if (roadType === 'Boulevard') return `${favoriteColor}way`;
+    if (roadType === 'Drive') return `${favoriteColor}wayfarer`;
+    return `${favoriteColor}gate`;
 }
 
 // Generate last name based on the last letter of the last name
 function generateLastName(lastName) {
     const lastLetter = lastName.charAt(lastName.length - 1).toLowerCase();
     switch (lastLetter) {
-        case 'a':
-            return 'Dawn';
-        case 'b':
-            return 'Storm';
-        case 'c':
-            return 'Blaze';
-        case 'd':
-            return 'Nightshade';
-        case 'e':
-            return 'Thunder';
-        case 'f':
-            return 'Fire';
-        case 'g':
-            return 'Hawk';
-        case 'h':
-            return 'Wolf';
-        case 'i':
-            return 'Frost';
-        case 'j':
-            return 'Shadow';
-        case 'k':
-            return 'Steel';
-        case 'l':
-            return 'Flare';
-        case 'm':
-            return 'Moon';
-        case 'n':
-            return 'Wind';
-        case 'o':
-            return 'Flame';
-        case 'p':
-            return 'Stormbringer';
-        case 'q':
-            return 'Star';
-        case 'r':
-            return 'Viper';
-        case 's':
-            return 'Valkyrie';
-        case 't':
-            return 'Cinder';
-        case 'u':
-            return 'Thunderstrike';
-        case 'v':
-            return 'Ember';
-        case 'w':
-            return 'Dragon';
-        case 'x':
-            return 'Nightfall';
-        case 'y':
-            return 'Starfire';
-        case 'z':
-            return 'Phoenix';
-        default:
-            return 'Moon';
+        case 'a': return 'Dawn';
+        case 'b': return 'Storm';
+        case 'c': return 'Blaze';
+        case 'd': return 'Nightshade';
+        case 'e': return 'Thunder';
+        case 'f': return 'Fire';
+        case 'g': return 'Hawk';
+        case 'h': return 'Wolf';
+        case 'i': return 'Frost';
+        case 'j': return 'Shadow';
+        case 'k': return 'Steel';
+        case 'l': return 'Flare';
+        case 'm': return 'Moon';
+        case 'n': return 'Wind';
+        case 'o': return 'Flame';
+        case 'p': return 'Stormbringer';
+        case 'q': return 'Star';
+        case 'r': return 'Viper';
+        case 's': return 'Valkyrie';
+        case 't': return 'Cinder';
+        case 'u': return 'Thunderstrike';
+        case 'v': return 'Ember';
+        case 'w': return 'Dragon';
+        case 'x': return 'Nightfall';
+        case 'y': return 'Starfire';
+        case 'z': return 'Phoenix';
+        default: return 'Moon';
     }
 }
 
 // Generate suffix based on the favorite animal
 function generateSuffix(favoriteAnimal) {
     switch (favoriteAnimal.toLowerCase()) {
-        case 'lion':
-            return 'of the Lion';
-        case 'tiger':
-            return 'of the Tiger';
-        case 'eagle':
-            return 'of the Eagle';
-        case 'dragon':
-            return 'of the Dragon';
-        case 'wolf':
-            return 'of the Wolf';
-        case 'phoenix':
-            return 'of the Phoenix';
-        case 'bear':
-            return 'of the Bear';
-        case 'snake':
-            return 'of the Serpent';
-        case 'unicorn':
-            return 'of the Unicorn';
-        case 'griffin':
-            return 'of the Griffin';
-        case 'swan':
-            return 'of the Swan';
-        case 'peacock':
-            return 'of the Peacock';
-        default:
-            return `of the ${favoriteAnimal}`;
+        case 'lion': return 'of the Lion';
+        case 'tiger': return 'of the Tiger';
+        case 'eagle': return 'of the Eagle';
+        case 'dog': return 'of the Dog';
+        case 'wolf': return 'of the Wolf';
+        case 'phoenix': return 'of the Phoenix';
+        case 'bear': return 'of the Bear';
+        case 'snake': return 'of the Serpent';
+        case 'cats': return 'of the Cats';
+        case 'bird': return 'of the Birds';
+        case 'horse': return 'of the Horses';
+        case 'peacock': return 'of the Peacock';
+        default: return `of the Hawks`;
     }
 }
 
@@ -217,4 +145,5 @@ function generateFullName() {
     // Display the full name in the result div
     const resultDiv = document.getElementById('result');
     resultDiv.textContent = fullName;
-    resultDiv.classList.add('result-style'); 
+    resultDiv.classList.add('result-style');
+}
